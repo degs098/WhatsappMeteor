eed76679864960699a6d98e6194381c87a752bb8
tolosita <jarboledatolosa@hotmail.com>
2016-06-23 22:36:49 -0500

    paso 6 y 7

--- whatsapp/client/scripts/routes.js
+++ whatsapp/client/scripts/routes.js
@@ -1,5 +1,5 @@
-
 import { _ } from 'meteor/underscore';
+import { Meteor } from 'meteor/meteor';
 import {Config, Runner} from 'angular-ecmascript/module-helpers';
 
 class RoutesConfig extends Config{
@@ -17,7 +17,10 @@ class RoutesConfig extends Config{
 				abstract: true,
 				templateUrl: 'client/templates/tabs.html',
 				resolve: {
-					user: this.isAuthorized
+					user: this.isAuthorized,
+		            chats() {
+            			return Meteor.subscribe('chats');
+          			}
 				}
 			})
 			.state('tab.chats', {
